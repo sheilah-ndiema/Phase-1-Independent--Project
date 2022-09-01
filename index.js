363e04050d8e804a89fe2d12931fbb1ac4c76fff
Ndiema-S <you@example.com>
2022-09-01 13:49:45 +0300

    add get all meals function

--- index.js
+++ index.js
@@ -71,3 +71,35 @@ function mealRecipeModal(meal) {
     mealDetailsContent.innerHTML = html;
     mealDetailsContent.parentElement.classList.add('showRecipe');
 }
+
+function getAllMeals() {
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=beef`)
+        .then(response => response.json())
+        .then(data => {
+            let html = "";
+            if (data.meals) {
+                data.meals.slice(0, 15).reverse().forEach(meal => {
+                    html += `
+                    <div class = "meal-item" data-id = "${meal.idMeal}">
+                        <div class = "meal-img">
+                            <img src = "${meal.strMealThumb}" alt = "food">
+                        </div>
+                        <div class = "meal-name">
+                            <h3>${meal.strMeal}</h3>
+                            <a href = "#" class = "recipe-btn">Get Recipe</a>
+                        </div>
+                    </div>
+                `;
+                });
+                mealList.classList.remove('notFound');
+            } else {
+                html = "Sorry, we didn't find any meal!";
+                mealList.classList.add('notFound');
+            }
+
+            mealList.innerHTML = html;
+        });
+}
+document.addEventListener("DOMContentLoaded", () => {
+    getAllMeals()
+})
