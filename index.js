a306b9657655de047072db2cf2d4db09db1f77cd
Ndiema-S <you@example.com>
2022-09-01 11:20:17 +0300

    add getMealRecipe function

--- index.js
+++ index.js
@@ -40,3 +40,14 @@ function getMealList() {
             mealList.innerHTML = html;
         });
 }
+
+// get recipe of the meal
+function getMealRecipe(e) {
+    e.preventDefault();
+    if (e.target.classList.contains('recipe-btn')) {
+        let mealItem = e.target.parentElement.parentElement;
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+            .then(response => response.json())
+            .then(data => mealRecipeModal(data.meals));
+    }
+}
