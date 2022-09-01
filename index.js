0bc0344262a92101f746414bfcdbdc8fb0f1363e
Ndiema-S <you@example.com>
2022-09-01 11:26:15 +0300

    add mealRecipeModal function

--- index.js
+++ index.js
@@ -51,3 +51,23 @@ function getMealRecipe(e) {
             .then(data => mealRecipeModal(data.meals));
     }
 }
+
+// create a modal
+function mealRecipeModal(meal) {
+    console.log(meal);
+    meal = meal[0];
+    let html = `
+        <h2 class = "recipe-title">${meal.strMeal}</h2>
+        <p class = "recipe-category">${meal.strCategory}</p>
+        <div class = "recipe-instruct">
+            <h3>Instructions:</h3>
+            <p>${meal.strInstructions}</p>
+        </div>
+        <div class = "recipe-meal-img">
+            <img src = "${meal.strMealThumb}" alt = "">
+        </div>
+        
+    `;
+    mealDetailsContent.innerHTML = html;
+    mealDetailsContent.parentElement.classList.add('showRecipe');
+}
